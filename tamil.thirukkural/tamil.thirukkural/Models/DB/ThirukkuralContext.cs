b21885166137aa4de52a75c94680da1430133d54
saravanan <saravanan.a@doxc.in>
2021-10-06 06:40:31 +0530

    Modified the connection string

--- tamil.thirukkural/tamil.thirukkural/Models/DB/ThirukkuralContext.cs
+++ tamil.thirukkural/tamil.thirukkural/Models/DB/ThirukkuralContext.cs
@@ -24,7 +24,7 @@ namespace Tamil.Thirukkural.Models.DB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=./.;Initial Catalog=Thirukkural;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;");
+                optionsBuilder.UseSqlServer("workstation id=thirukkuraldb.mssql.somee.com;packet size=4096;user id=saravana1310_SQLLogin_1;pwd=f4eiz59a3x;data source=thirukkuraldb.mssql.somee.com;persist security info=False;initial catalog=thirukkuraldb");
             }
         }
 
